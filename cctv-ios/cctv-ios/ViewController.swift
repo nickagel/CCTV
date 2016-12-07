8db30b4d2fbc08a696aeb123e8d7b2c28130e746
Lily Nguyen <lilynguyen@ip0af50eed.int.uvm.edu>
2016-12-07 03:11:39 -0500

    var chnges in storyboard, added commented test code

--- cctv-ios/cctv-ios/ViewController.swift
+++ cctv-ios/cctv-ios/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import EVReflection
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     let repository = RepositoryRetrieval()
     var locals = Locals()
     // 
     @IBOutlet weak var tableArchiveView: UITableView!
     
+//    var titleArray = [String]()
+    
+//     Test data for Archives
     var titles = ["title1","title2","title3"]
     var subtitles = ["sub1","sub2","sub3"]
     var images = [UIImage(named:"Video Thumb Placeholder"),UIImage(named:"Video Thumb Placeholder"),UIImage(named:"Video Thumb Placeholder")]
@@ -26,6 +30,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var initialize = self.repository.Initialize()
         self.locals = initialize
         print(self.locals)
+        //print(self.locals.archives)
+        
+        //var i = 0
+        //for j in self.locals.archives {
+        //    if i == 3 {
+        //        print(j.nid!)
+        //    }
+        //    i += 1
+        //}
+        
+        for j in self.locals.archives {
+            //print(j.title!)
+//            self.titleArray.append(j.title!)
+        }
+        
+//        print(self.titleArray)
         
         //var exampleSearch = self.repository.SearchArchives(search:"Burlington")
     
@@ -39,17 +59,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ArchiveTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "archiveCell", for: indexPath) as! ArchiveTableViewCell
 
         cell.thumbView.image = images[indexPath.row]
         cell.titleLabel.text = titles[indexPath.row]
         cell.subtitleLabel.text = subtitles[indexPath.row]
         cell.timeLabel.text = times[indexPath.row]
+        
+//        cell.thumbView.image = UIImage(named:"Video Thumb Placeholder")
+        
+        // loop through to access the "indexPath"th item in self.locals.archives
+        //        print(self.locals.archives)
+        
+        //        var i = 0
+        //        if self.locals.archives == nil {
+        //            print("fuck")
+        //        } else {
+        //            for item in self.locals.archives {
+        //                if i == 3 { //indexPath.row
+        //                    print(item.nid!) // set the shit here
+        //                }
+        //                i += 1
+        //            }
+        //        }
+        
+        
+//        for j in self.locals.archives {
+//            print(j.nid!)
+//        }
+        
+        //        print(titleArray[indexPath.row+1])
+        //        cell.titleLabel.text = titleArray[indexPath.row]
             
         return cell
     }
