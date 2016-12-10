777923f9f5435a613ac43753ac7d7aa26b3664fc
Hamjay <jason.es.hammel@gmail.com>
2016-12-10 14:43:42 -0500

    Add files via upload

--- cctv-android/main/java/com/example/hambrooks/ccvt/DisplayCh17Activity.java
+++ cctv-android/main/java/com/example/hambrooks/ccvt/DisplayCh17Activity.java
@@ -1,41 +1,64 @@
 package com.example.hambrooks.ccvt;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.VideoView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import android.util.*;
+
 import android.net.Uri;
 import android.widget.MediaController;
+import android.widget.VideoView;
+
 public class DisplayCh17Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_ch17);
+//
+//        Intent intent = getIntent();
+//        TextView textView = new TextView(this);
+//        textView.setTextSize(40);
+//
+//
+//
+//        textView.setText("CH17");
+//        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_ch17);
+//        layout.addView(textView);
 
-        Intent intent = getIntent();
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText("17 I HEART chocolate");
+        try {
+            VideoView vidView = (VideoView) findViewById(R.id.myVideo);
 
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_ch17);
-        layout.addView(textView);
+//            vidView.setVideoPath(
+//                    "http://www.ebookfrenzy.com/android_book/movie.mp4");
+//
+//            vidView.start();
+             //  String vidAddress = "http://www.ebookfrenzy.com/android_book/movie.mp4";
+           //    String vidAddress = "http://camendesign.com/code/video_for_everybody/test.html";
+             //  String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+            //  String vidAddress = "https://archive.org/details/Largemoviefiletest";
+              //String vidAddress = "https://www.cctv.org/stream-player-build?nid=171920";// "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+            String vidAddress = "https://media2.cctv.org/cctv/library/2016/10/DebateSundayAttorneyGeneral_F_10232016/DebateSundayAttorneyGeneral_F_10232016.broadband.mp4";
+            Uri vidUri = Uri.parse(vidAddress);
+            MediaController vidControl = new MediaController(this);
+            vidControl.setAnchorView(vidView);
+            vidView.setMediaController(vidControl);
+            vidView.setVideoURI(vidUri);
+            vidView.start();
+        }catch(Exception e){
+            Log.d("FAIL","Caught Exception");
+        }
+
+
 
 
     }
 }
 
 
-//    VideoView vidView = (VideoView)findViewById(R.id.myVideo);
-//
-//    String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
-//    Uri vidUri = Uri.parse(vidAddress);
-//     vidView.setVideoURI(vidUri);
-//
-//        MediaController vidControl = new MediaController(this);
-//        vidControl.setAnchorView(vidView);
-//        vidView.setMediaController(vidControl);
-//        vidView.start();
